2947b22680cd24c0727c96a1a90559f174789b48
Laura DiSanza <ldisanza7@gmail.com>
2021-04-10 19:22:30 -0500

    More JS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
  var rock = document.querySelector("#rock");
  var scissors = document.querySelector("#scissors");
  var paper = document.querySelector("#paper");
+ var options = document.querySelector(".options");
 
 
 /* Computer's choice */
@@ -10,3 +11,10 @@ var computerChoice = computerOptions[Math.floor(Math.random()*3)];
 
 /* Now the conditions for win, lose or draw */
 /* are we doing a function with a click event? with all of the ifs nestled on the inside? */
+
+rock.addEventListener("click", function () {
+    if (computerChoice === 'rock') {
+        options.classList.remove("options-hide");
+        /* show .results class */
+    }
+})
